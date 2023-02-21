f0da7b214cad5e5c1068ad62cc0a60431af45a68
ReneFuentes <fuentesbenitezreneismael>
2023-02-21 10:20:55 -0600

    Proyectopara consumir API

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <Navbar brand-name="Countries API"/>
+  <router-view />
+</template>
+
+<script>
+import Navbar from '../src/components/Navbar/index.vue'
+
+export default({
+  components:({
+    Navbar
+  })
+})
+</script>
---
+++ src/router.js
@@ -0,0 +1,19 @@
+import { createRouter, createWebHistory} from 'vue-router';
+import Home from './views/home.vue';
+import Countries from './views/countries/index.vue';
+export default createRouter({
+    history: createWebHistory(),
+    routes:[
+        {
+            path:'/',
+            name: 'Home',
+            component: Home
+        },
+        {
+            path:'/countries',
+            name:'Countries',
+            component: Countries
+        }
+
+    ]
+})
